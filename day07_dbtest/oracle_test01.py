8956d0e68f5965fd9cc3430f67d748b78cbd7ec7
gpwls2014 <gpwls2014@naver.com>
2022-05-23 14:51:40 +0900

    2022/5/23 오라클 라이브러리 설치 내용 추가

--- day07_dbtest/oracle_test01.py
+++ day07_dbtest/oracle_test01.py
@@ -2,30 +2,11 @@ import cx_Oracle
 
 conn = cx_Oracle.connect('scott','tiger','localhost:1521/xe')
 cursor = conn.cursor()
-cursor.execute("select * from emp")
+cursor.execute("select * from emp where deptno = 10")
 
 # print(cursor)
 
-while True:
-    choice=input('''
-다음 중에서 하실 일을 골라주세요 :
-1-job 검색 2-사원이름 검색
->>>''')
+for item in cursor:
+    print(item[1],item[5])
 
-    if choice == '1':
-        item={'empno':'','ename':'','job':'','mgr':'','hiredate':'','sal':'','comm':'','deptno':''}
-        for item in cursor:
-            item['job']=input('job >>>')
-            print(item[1],item[2])
-    
-    else:
-        item['ename']=input('이름 입력>>>')
-        print(item[1])
-    
-    conn.close()
-
-
-# 1.emp 테이블에 job을 입력받아 해당 job인 사원을 출력하세요.
-# 2.사원의 이름의 일부를 입력받아서 검색해서 출력 
-
-
+conn.close()
